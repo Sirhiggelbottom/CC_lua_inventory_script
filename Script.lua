75b44a301d71d47b6e1a8ffa926a120e5c220744
Sirhiggelbottom <147952918+Sirhiggelbottom@users.noreply.github.com>
2023-12-30 23:56:08 +0100

    Added more debugging

--- Script.lua
+++ Script.lua
@@ -2,6 +2,9 @@ local chest1 = peripheral.wrap("minecraft:chest_0")
 local chest2 = peripheral.wrap("minecraft:chest_1")
 local monitor = peripheral.wrap("monitor_2")
 
+local chest1Size = chest1.size()
+local chest2Size = chest2.size()
+
 local function debug(text)
     monitor.clear()
     monitor.setCursorPos(1,1)
@@ -28,6 +31,19 @@ while true do
         local resultIron = chest1.pushItems(peripheral.getName(chest2), ironSlot, 1)
         local resultGold = chest1.pushItems(peripheral.getName(chest2), goldSlot, 1)
         debug("Moved iron: " .. resultIron .. "\nMoved gold: " .. resultGold)
+    
+    elseif chest1Size < 1 and chest2Size < 1 then
+        debug("Chests aren't connected")
+    end
+    
+    elseif chest1Size < 1 then
+        debug("Chest 1 isn't connected")
+    end
+    
+    elseif chest2Size < 1 then
+        debug("Chest 2 isn't connected")
+    end
+    
     else
         debug("Iron or gold not found.")
     end
