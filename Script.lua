adfcf51c4b66bf47ea55c7ddec9395c11b39b9ae
Sirhiggelbottom <147952918+Sirhiggelbottom@users.noreply.github.com>
2023-12-30 23:47:06 +0100

    Added debugging

--- Script.lua
+++ Script.lua
@@ -1,12 +1,20 @@
 local chest1 = peripheral.wrap("minecraft:chest_0")
 local chest2 = peripheral.wrap("minecraft:chest_1")
+local monitor = peripheral.wrap("monitor_2")
+
+local function debug(text)
+    monitor.clear()
+    monitor.setCursorPos(1,1)
+    monitor.write(text)
+end
 
 while true do
     local hasIron = false
     local hasGold = false
     local ironSlot, goldSlot
 
-    for slot, item in pairs(chest1.list()) do
+    local items = chest1.list()
+    for slot, item in pairs(items) do
         if item.tags and item.tags["forge:ingots/iron"] then
             hasIron = true
             ironSlot = slot
@@ -17,8 +25,11 @@ while true do
     end
 
     if hasIron and hasGold then
-        chest1.pushItems(peripheral.getName(chest2), ironSlot, 1)
-        chest1.pushItems(peripheral.getName(chest2), goldSlot, 1)
+        local resultIron = chest1.pushItems(peripheral.getName(chest2), ironSlot, 1)
+        local resultGold = chest1.pushItems(peripheral.getName(chest2), goldSlot, 1)
+        debug("Moved iron: " .. resultIron .. "\nMoved gold: " .. resultGold)
+    else
+        debug("Iron or gold not found.")
     end
 
     os.sleep(4)
