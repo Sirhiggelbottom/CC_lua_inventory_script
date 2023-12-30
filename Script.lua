8f1186792dbcef2eac04c7ca1e3c7d488c295a89
Sirhiggelbottom <147952918+Sirhiggelbottom@users.noreply.github.com>
2023-12-31 00:14:11 +0100

    Change some logic

--- Script.lua
+++ Script.lua
@@ -18,35 +18,35 @@ while true do
 
     local items = chest1.list()
     for slot, item in pairs(items) do
-        if item.tags and item.tags["forge:ingots/iron"] then
+        if item.name == "minecraft:iron_ingot" then
             hasIron = true
             ironSlot = slot
-        elseif item.tags and item.tags["forge:ingots/gold"] then
+        elseif item.name == "minecraft:gold_ingot" then
             hasGold = true
             goldSlot = slot
         end
     end
 
-    if hasIron and hasGold then
+    if hasIron and hasGold and chest2.size() < 2 then
         local resultIron = chest1.pushItems(peripheral.getName(chest2), ironSlot, 1)
         local resultGold = chest1.pushItems(peripheral.getName(chest2), goldSlot, 1)
         debug("Moved iron: " .. resultIron .. "\nMoved gold: " .. resultGold)
-    end
-    
+    break
+
     elseif chest1Size < 1 and chest2Size < 1 then
         debug("Chests aren't connected")
-    end
+    break
     
     elseif chest1Size < 1 then
         debug("Chest 1 isn't connected")
-    end
+    break
     
     elseif chest2Size < 1 then
         debug("Chest 2 isn't connected")
-    end
+    break
     
     else
-        debug("Iron or gold not found.")
+        debug("Iron or gold not found, or Chest2 already have ingots.")
     end
 
     os.sleep(4)
